b85beffeae6c85313f8aaa23abb8e3184294d883
jossichi244-ops <jossichi244@gmail.com>
2025-09-11 12:20:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,19 @@ app = FastAPI(title="Web3 Auth + Users API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://2244jlsh-5173.asse.devtunnels.ms/",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Healthcheck route
+@app.get("/")
+def root():
+    return {"message": "API is running 🚀"}
 
 app.include_router(auth_routes.router)
 app.include_router(user_routes.router)
@@ -18,4 +26,5 @@ app.include_router(task_routes.router)
 app.include_router(group_routes.router)
 app.include_router(group_member_routes.router) 
 app.include_router(task_comment_routes.router)
+
 app.include_router(attachment_verification_rouytes.router)
