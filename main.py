f53c67ab8917d84f645acb90221063b7a38b8de7
jossichi <bloc907@gmail.com>
2025-09-11 20:11:28 +0700

    ass

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://2244jlsh-5173.asse.devtunnels.ms",
+        "https://naver-hackathon-jossichi.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
